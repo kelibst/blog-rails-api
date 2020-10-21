325e9787d3f14e8f372f88df84d48aef19f98df7
Keli Booster <kbooster17@gmail.com>
2020-10-21 21:54:34 +0000

    add more controller test for articles

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'factory_bot_rails'
   gem 'rspec-rails'
+  gem 'active_model_serializers'
 end
 
 group :development do
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    sequence(:title) { |n| "The Tittle of my awesome blog #{n}" }
+    sequence(:content) { |n| "The content of my awesome blog #{n}"}
+    sequence(:slug) { |n| "my_awesome_title_#{n} "}
   end
 end
